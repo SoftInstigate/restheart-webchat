439c2015e01a86fb2cafd34980c76870ab2b9d6b
Maurizio Turatti <maurizio@softinstigate.com>
2021-07-15 19:45:55 +0200

    Fix production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  SENTRY_DSN: "https://266665e29f964150b4f62aa9395046be@o912054.ingest.sentry.io/5848878",
-  SENTRY_TRACING_ORIGINS: ["localhost"]
+  MESSAGE_FEED: 'wss://demo.restheart.org/messages/_streams/all',
+  MESSAGE_URL: 'https://demo.restheart.org/messages',
+  SENTRY_DSN: 'https://266665e29f964150b4f62aa9395046be@o912054.ingest.sentry.io/5848878',
+  SENTRY_TRACING_ORIGINS: ['chat.restheart.org'],
+  RECONNECTION_DELAY: 3000
 };
