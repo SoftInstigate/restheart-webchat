9e5a138a499371f4f5f5b2f58140099d01c6c019
stas-antokhi <stas@softinstigate.com>
2021-07-15 11:53:08 +0200

    Messages are now grouped by date and each group is separated by a day divider

--- src/app/components/messages-container/messages-container.component.ts
+++ src/app/components/messages-container/messages-container.component.ts
@@ -10,9 +10,14 @@ import {
 import { MessageService } from '../../services/message.service';
 import { MessageComponent } from '../message/message.component';
 import { Message } from '../../models/message';
-import { scan, startWith} from 'rxjs/operators';
-import { Observable} from 'rxjs';
+import {filter, groupBy, map, mergeMap, scan, startWith, switchMap, tap, toArray} from 'rxjs/operators';
+import {from, GroupedObservable, iif, Observable} from 'rxjs';
 import {StatusService} from '../../services/status.service';
+
+interface MessageGroups {
+  date: Date;
+  messages: Message[]
+}
 
 @Component({
   selector: 'app-messages-container',
@@ -23,11 +28,13 @@ export class MessagesContainerComponent implements OnInit, AfterViewInit {
   @ViewChild('flag') private flag: ElementRef<HTMLDivElement>;
   @ViewChildren('msg') private containerChildren: QueryList<MessageComponent>;
 
-  messages$: Observable<Message[]>;
+  messages$: Observable<MessageGroups[]>;
   currentPage = 1;
   firstElement: MessageComponent;
 
   loadMoreAction = false;
+  hasMoreMessagesToLoad = true;
+  messageCount = 0;
 
   constructor(
     private messageService: MessageService,
@@ -36,8 +43,28 @@ export class MessagesContainerComponent implements OnInit, AfterViewInit {
   ) { }
 
   ngOnInit(): void {
-
-    this.messages$ = this.messageService.getChatMessages();
+    /*
+    Note: reduce() === toArray()
+    Given a stream of messages group them by date. Ignore all messages without date
+    Ex: [msg1, msg2,...] => [ {date: '12-04-2021', messages: [msg1, ...] }, {key: '12-05-2021', messages: [msg5, ...] }]
+     */
+    this.messages$ = this.messageService.getChatMessages().pipe(
+      // given an observable of messages[] get rid of those without a timestamp
+      map((message: Message[]) => message.filter(m => m.timestamp && m.timestamp.$date)),
+      switchMap((messages: Message[]) => from(messages).pipe(
+        groupBy(
+          ({timestamp}) => (new Date(timestamp.$date)).toDateString(),
+            message => message
+        ),
+        mergeMap((group$: GroupedObservable<string, Message>) =>
+          group$.pipe(
+            toArray(),  // equivalent to reduce() -> from groupedObservable of messages to array of messages
+            map((messageArray: Message[]): MessageGroups => ({ date: new Date(group$.key), messages: messageArray}))
+          )
+        ),
+        toArray() // wait for all groups to be emitted and collect them in one single array
+      ))
+    );
   }
 
   ngAfterViewInit(): void {
